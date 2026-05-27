555c8a4ba9f40b2ecb66e1c1e6631afde6d637d5
Umair Ashraf <umrashrf@gmail.com>
2026-05-27 13:28:58 -0400

    Auto backup git projects to storage

--- backup.sh
+++ backup.sh
@@ -3,6 +3,16 @@
 DEST="/Volumes/SANDISK/$(hostname)"
 
 echo 'Starting backup..' &&
+  echo "Finding and running git push" &&
+    find ~/Develop -maxdepth 3 -mindepth 1 -type d | while read -r dir; do
+      if [ -d "$dir/.git" ]; then
+        remote_url=$(git -C "$dir" config --get remote.origin.url)
+        if [[ "$remote_url" == /Volumes/* ]]; then
+          echo "Pushing $dir (Remote: $remote_url)"
+          git -C "$dir" push mypassport
+        fi
+      fi
+    done &&
     rsync -avh --delete ~/Desktop "$DEST" &&
     rsync -avh --delete ~/Documents "$DEST" &&
     rsync -avh --remove-source-files ~/Downloads "$DEST" &&
